9a49a8761a54fe1678b032d944c4a97c532048a9
JustOff <Off.Just.Off@gmail.com>
2021-02-22 19:03:37 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,12 @@
+@echo off
+set VER=1.3.3
+
+sed -i -E "s/\"version\": \".+?\"/\"version\": \"%VER%\"/; s/\"name\": \".+?\"/\"name\": \"pmforum-smart-preview-%VER%\"/" package.json
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/pmforum-smart-preview-.+?\.xpi/download\/%VER%\/pmforum-smart-preview-%VER%\.xpi/" update.xml
+
+set XPI=pmforum-smart-preview-%VER%.xpi
+if exist %XPI% del %XPI%
+if exist bootstrap.js del bootstrap.js
+if exist install.rdf del install.rdf
+call jpm xpi
+unzip %XPI% bootstrap.js install.rdf
